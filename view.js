39fcfe36acd42bf4b8aa08e468cef2ba5c46f9e2
gshanske <gshanske@ohryisrael.com>
2015-01-15 16:52:02 -0500

    Update view.js

--- view.js
+++ view.js
@@ -1,105 +1,141 @@
-var canvas = document.getElementById('canvas');
-  var ctx = canvas.getContext("2d");
+var app = {};
+ 
+app.init = function () {
+  app.canvas = document.getElementById('canvas');
+  app.ctx = app.canvas.getContext("2d");
+  app.projectile = new Image();
+  app.projectile.src = 'Cannonball.png';
+  app.healthLeft = 100;
+  app.movementLeft = 10;
+  app.hasHitEnemy;
+  app.hasHitRightWall;
+  app.hasHitLeftWall;
+  app.Radians = 0;
+  app.Position = 3*window.innerWidth/50;
+  app.shotInMotion = false;
+  app.shotPower = 1;
+  app.secondsInAir = 0;
+  app.gravity = -9.8*app.secondsInAir;
+ 
+  app.resize();
+  app.moveTank('p1');
   
+  return app;
+};
   
-  var resize = function () {
-      ctx.canvas.width  = window.innerWidth;
-      ctx.canvas.height = window.innerHeight;
-    };
+app.resize = function () {
+  app.ctx.canvas.width  = window.innerWidth;
+  app.ctx.canvas.height = window.innerHeight;
+  app.tankWidth = window.innerWidth/11;
+  app.tankHeight = window.innerHeight/10;
+  app.turretWidth = window.innerWidth/15;
+  app.turretHeight = window.innerHeight/75;
+  return app;
+};
   
-  var tankWidth = window.innerWidth/11;
-  var tankHeight = window.innerHeight/10;
-  var turretWidth = window.innerWidth/15;
-  var turretHeight = window.innerHeight/75;
-  
-  // Draws the tank.
+// Draws the tank.
+app.moveTank = function(player){
+  if(player === 'p1'){
+    app.ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    app.ctx.save();
+    app.ctx.translate(app.Position, 3.75 * window.innerHeight / 7);
+    app.ctx.fillStyle = "#00ff00";
+    
+    // Draws the tank body.
+    
+    app.ctx.fillRect(0, 0, app.tankWidth, app.tankHeight);
+    
+    // Draws the tank's turret.
+    
+    app.ctx.translate(window.innerWidth / 15,0);
+    app.ctx.rotate(app.Radians);
+    app.ctx.fillRect(0,0,app.turretWidth,app.turretHeight);
+    app.ctx.restore();
+    
+    return app;
+  }
+};
 
-  var moveTank = function(position,angle){
-    ctx.clearRect(0,0,window.innerWidth,window.innerHeight);
-    ctx.save();
-    ctx.translate(position,3.75*window.innerHeight/7);
-    ctx.fillStyle = "#00ff00";
+app.findShotX = function(time){
+  app.secondsInAir += 0.1;
+  return Math.cos(app.Radians)*app.shotPower*(time-0.1);
+};
 
-  // Draws the tank body.
+app.findShotY = function(time){
+  return Math.sin(app.Radians)*app.shotPower*(time-0.1);
+};
+
+app.shoot = function(){
+  app.moveTank('p1');
+  app.ctx.save();
+  app.ctx.translate(app.Position + window.innerWidth/11+window.innerWidth/15,3.75*window.innerHeight/7);
+  app.ctx.drawImage(app.projectile,app.findShotX(app.secondsInAir),app.findShotY(app.secondsInAir),window.innerHeight/30,window.innerHeight/30);
+  if (app.findShotY(app.secondsInAir) === 0){
+   app.shotInMotion = false;
+  }
+  app.ctx.restore();
   
-    ctx.fillRect(0,0,tankWidth,tankHeight);
+} ;
   
-  // Draws the tank's turret.
-   
-    ctx.translate(window.innerWidth/15,0);
-    ctx.rotate(angle);
-    ctx.fillRect(0,0,turretWidth,turretHeight);
-    ctx.restore();
-  };
-  
-  var healthLeft = 100;
-  var movementLeft = 10;
-  var hasHitEnemy;
-  var hasHitRightWall;
-  var hasHitLeftWall;
-  var radians = 0;
-  var pos = 3*window.innerWidth/50;
-  
-  moveTank(pos,radians*Math.Pi);
   
   // Binds the movement and aim of the tank to the arrow keys.
   
-    document.addEventListener('keydown', function (e) {
-      
-      hasHitRightWall = false;
-      hasHitLeftWall = false;
-      
-      if(pos <= 0){
-        hasHitLeftWall = true;
-        }
-      
-      if(pos >= window.innerWidth-tankWidth-turretWidth){
-        hasHitRightWall = true;
-        }
-      
-      switch (e.keyCode){
-      
-      // Left
-        case 37:
-          resize();
-          if(!hasHitLeftWall){
-            pos -= 2.5;
-          }
-          moveTank(pos,radians*Math.PI);
-          break;
-      // Up
-        case 38:
-          resize();
-          if(radians%2 > -0.35){
-          radians -= 0.15;
-          }
-          moveTank(pos,radians*Math.PI);
-          break;
-      // Right
-        case 39:
-          resize();
-          if(!hasHitRightWall){
-          pos += 2.5;
-          }
-          moveTank(pos,radians*Math.PI);
-          break;
-      // Down
-        case 40:
-          resize();
-         if(radians%2 < 0){
-          radians += 0.15;
-         }
-          moveTank(pos,radians*Math.PI);
-          break;
-      // Space
-        case 32:
-          resize();
-         // Code to shoot goes here
-          break;
-        
-        default:
-          resize();
-          break;
+document.addEventListener('keydown', function (e) {
+
+  hasHitRightWall = false;
+  hasHitLeftWall = false;
     
-      }
-    });
+  if(app.Position <= 0){
+    hasHitLeftWall = true;
+  }
+  
+  if(app.Position >= window.innerWidth-app.tankWidth-app.turretWidth){
+    hasHitRightWall = true;
+  }
+    
+    switch (e.keyCode){
+  
+    // Left
+    case 37:
+    if(!hasHitLeftWall){
+    app.Position -= 2.5;
+  }
+    app.moveTank('p1');
+    break;
+    // Up
+    case 38:
+    if(app.Radians%2 > -0.35){
+    app.Radians -= 0.15;
+  }
+    app.moveTank('p1');
+    break;
+    // Right
+    case 39:
+    if(!hasHitRightWall){
+    app.Position += 2.5;
+  }
+    app.moveTank('p1');
+    break;
+    // Down
+    case 40:
+    if(app.Radians%2 < 0){
+    app.Radians += 0.15;
+  }
+    app.moveTank('p1');
+    break;
+    // Space
+    case 32:
+    app.shotInMotion = true;
+    while (app.shotInMotion){
+    app.shoot();
+    }
+    break;
+    
+    default:
+    app.moveTank('p1');
+    break;
+  
+  }
+});
+window.onresize = app.resize;
+app.init();
